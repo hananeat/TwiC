1d9210fe4fa639a6e317461572d1d19e4b9879ed
Mame Diarra <mangiara27@gmail.com>
2026-06-06 20:09:44 +0200

    Implementata schermata delle statistiche interattiva

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import path_provider_foundation
-=======
->>>>>>> 40cef0456f424007c3bb7923aafe00b16af4bbc4
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
