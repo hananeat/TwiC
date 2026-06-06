c1353175f0f88854c7e17853534cbe816924946d
Mame Diarra <mangiara27@gmail.com>
2026-06-06 19:47:34 +0200

    salvo modifiche locali

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import path_provider_foundation
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-=======
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
->>>>>>> 7312bdfd93652732acf25ef57356e428539f537e
 }
