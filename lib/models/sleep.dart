5f02dac551edab331ed9a2512c050e8cdf39df9a
Hanane Ait Tioui <hananeaittioui@gmail.com>
2026-06-21 10:47:43 +0200

    dati aggiunti: sleep e step; aggiunte anche le rispettive funzioni su impact.dart

---
+++ lib/models/sleep.dart
@@ -0,0 +1,44 @@
+import 'package:intl/intl.dart';
+
+class Sleep {
+  
+  // Instance variables
+  final DateTime date;
+  final int minutesAsleep;
+  final int efficiency;
+  
+  // Sleep stage details in minutes
+  final int deepMinutes;
+  final int wakeMinutes;
+  final int lightMinutes;
+  final int remMinutes;
+
+  // Constructor
+  Sleep({
+    required this.date,
+    required this.minutesAsleep,
+    required this.efficiency,
+    required this.deepMinutes,
+    required this.wakeMinutes,
+    required this.lightMinutes,
+    required this.remMinutes,
+  });
+
+  // Constructor that converts the API JSON directly into a Sleep object
+  Sleep.fromJson(String queryDate, Map<String, dynamic> json)
+      : date = DateFormat('yyyy-MM-dd').parse(queryDate),
+
+      // ?? operator = if the value is null, use the value after ??
+        minutesAsleep = int.parse(json["minutesAsleep"]?.toString() ?? "0"),
+        efficiency = int.parse(json["efficiency"]?.toString() ?? "0"),
+        deepMinutes = int.parse(json["levels"]?["summary"]?["deep"]?["minutes"]?.toString() ?? "0"),
+        wakeMinutes = int.parse(json["levels"]?["summary"]?["wake"]?["minutes"]?.toString() ?? "0"),
+        lightMinutes = int.parse(json["levels"]?["summary"]?["light"]?["minutes"]?.toString() ?? "0"),
+        remMinutes = int.parse(json["levels"]?["summary"]?["rem"]?["minutes"]?.toString() ?? "0");
+
+  @override
+  String toString() {
+    return 'Sleep{date: $date, minutesAsleep: $minutesAsleep, efficiency: $efficiency}';
+  }
+
+}
